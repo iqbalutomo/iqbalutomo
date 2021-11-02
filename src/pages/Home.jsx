ba1b18c60432310779e9fb2af2e98dba939ee6fc
Muhlis Iqbal Utomo <muhlisiqbalutomo@gmail.com>
2021-11-02 18:56:03 +0700

    Slicing navbar component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,73 +1,11 @@
 import React from "react";
 import "@google/model-viewer";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <>
-      <section>
-        <nav class="fixed z-10 w-full backdrop-filter backdrop-blur-md shadow-md">
-          <div class="max-w-6xl mx-auto px-4">
-            <div class="flex justify-between">
-              <div>
-                <a href="#" class="flex items-center py-4 px-2">
-                  <span class="font-semibold text=black text-lg">iqbalutomo</span>
-                </a>
-              </div>
-              <div id="my-nav" class="hidden md:flex space-x-4 items-center">
-                <a id="about-nav" class="btn py-3 px-2 rounded active-nav" href="#about-section">
-                  About
-                </a>
-                <a id="works-nav" class="btn py-3 px-2 rounded" href="#works-section">
-                  {" "}
-                  Works{" "}
-                </a>
-                <a id="link-nav" class="btn py-3 px-2 rounded" href="#link-section">
-                  {" "}
-                  Link{" "}
-                </a>
-              </div>
-              <div class="md:flex inline-flex items-center space-x-3">
-                <a href="https://github.com/iqbalutomo" target="blank">
-                  <img src="./assets/icon/GitHub-Mark-32px.png" alt="GitHub" width="25" />
-                </a>
-                <button type="button" class="sun py-2 px-2 font-medium rounded bg-purple-300 hover:bg-purple-200 hover:text-yellow-100 transition duration-100">
-                  <img src="./assets/icon/sun-light-theme-svgrepo-com.svg" width="20" />
-                </button>
-                <button type="button" class="moon py-2 px-2 font-medium rounded bg-purple-300 hover:bg-purple-200 hover:text-yellow-100 transition duration-100">
-                  <img src="./assets/icon/moon-dark-theme-svgrepo-com.svg" width="20" />
-                </button>
-              </div>
-              <div class="md:hidden flex items-center">
-                <button class="outline-none mobile-menu-button">
-                  <svg class="w-6 h-6 text-gray-500 hover:text-gray-500" x-show="!showMenu" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                    <path d="M4 6h16M4 12h16M4 18h16"></path>
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div class="hidden mobile-menu">
-            <ul class="">
-              <li class="active">
-                <a href="#about-section" class="block text-sm px-2 py-4 text-white brown font-semibold">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#works-section" class="block text-sm px-2 py-4 hover:brown transition duration-100">
-                  Works
-                </a>
-              </li>
-              <li>
-                <a href="#link-section" class="block text-sm px-2 py-4 hover:brown transition duration-100">
-                  Link
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </section>
+      <Navbar />
 
       <section id="about-section">
         <div class="max-w-6xl mx-auto px-4">
