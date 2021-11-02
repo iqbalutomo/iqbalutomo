3fd19643cbcde0c016eb48e91259c308d744e980
Muhlis Iqbal Utomo <muhlisiqbalutomo@gmail.com>
2021-11-03 06:09:16 +0700

    Add icon lindkedin

--- src/components/Contact/ListLink.jsx
+++ src/components/Contact/ListLink.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ListLink(props) {
   const { title, icon, width, height, alt, link } = props;
   return (
-    <a href={link} className="flex md:inline-flex items-center gap-2 py-3 px-3 rounded-md border-2 border-khaki dark:border-grey hover:underline">
+    <a href={link} className="flex md:inline-flex items-center gap-2 py-3 px-3 rounded-md border-2 border-khaki dark:border-grey hover:underline" target="_blank" rel="noreferrer">
       <img src={icon} width={width} height={height} alt={alt} className="dark:bg-white rounded-md dark:border-2 dark:border-white" />
       <h4>{title}</h4>
     </a>
--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -3,6 +3,7 @@ import ListLink from "./ListLink";
 import icGithub from "../../assets/icons/Link/ic-github.png";
 import icEmail from "../../assets/icons/Link/ic-email.png";
 import icInstagram from "../../assets/icons/Link/ic-instagram.png";
+import icLinkedin from '../../assets/icons/Link/ic-linkedin.png'
 
 export default function Contact() {
   return (
@@ -10,8 +11,9 @@ export default function Contact() {
       <h1 className="font-bold text-xl text-center mb-8 dark:text-pink-400">Link</h1>
       <div className="flex flex-wrap justify-center gap-4">
         <ListLink title="@iqbalutomo" icon={icGithub} width="26" height="26" alt="GitHub" link="https://github.com/iqbalutomo" />
-        <ListLink title="muhlisiqbalutomo@gmail.com" icon={icEmail} width="24" height="24" alt="Email" link="mailto:muhlisiqbalutomo@gmail.com" />
+        <ListLink title="Muhlis Iqbal Utomo" icon={icLinkedin} width="26" height="26" alt="LinkedIn" link="https://www.linkedin.com/in/muhlis-iqbal-utomo-2257a2224" />
         <ListLink title="@iqbalutomo" icon={icInstagram} width="24" height="24" alt="Instagram" link="https://www.instagram.com/iqbalutomo/" />
+        <ListLink title="muhlisiqbalutomo@gmail.com" icon={icEmail} width="24" height="24" alt="Email" link="mailto:muhlisiqbalutomo@gmail.com" />
       </div>
     </div>
   );
